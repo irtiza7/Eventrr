150e8d655763af0cd6d79d5f1191f935a2ba9b25
Muhammad Irtiza <m7.irtiza@gmail.com>
2024-07-30 23:01:54 +0500

    Initial Commit: * Created empty project.

---
+++ Eventrr/Eventrr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eventrr
+//
+//  Created by Dev on 7/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Eventrr/EventrrUITests/EventrrUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EventrrUITestsLaunchTests.swift
+//  EventrrUITests
+//
+//  Created by Dev on 7/30/24.
+//
+
+import XCTest
+
+final class EventrrUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
